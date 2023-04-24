62901791d5f906c80aae8c02aa62042001451fc5
yanqui <20507951+yanqui-uxo@users.noreply.github.com>
2023-04-23 22:05:56 -0700

    Do a little shifty-wifty

--- lib/logic/game_timer.dart
+++ lib/logic/game_timer.dart
@@ -4,33 +4,29 @@ import 'package:flutter/foundation.dart';
 class GameTimer with ChangeNotifier {
   static const tickMilliseconds = 50;
   final Stopwatch _stopwatch = Stopwatch();
-  RestartableTimer? _timer;
+  late final RestartableTimer _timer;
 
   bool get active => _stopwatch.isRunning;
 
   Duration get elapsed => _stopwatch.elapsed;
 
   GameTimer() {
-    _timer = RestartableTimer(
-        const Duration(milliseconds: tickMilliseconds), notifyListeners);
+    _timer =
+        RestartableTimer(const Duration(milliseconds: tickMilliseconds), () {
+      notifyListeners();
+      _timer.reset();
+    });
+    _timer.cancel();
   }
 
   void start() {
     _stopwatch.start();
-
-    late RestartableTimer sampleTimer;
-    sampleTimer =
-        RestartableTimer(const Duration(milliseconds: tickMilliseconds), () {
-      notifyListeners();
-      sampleTimer.reset();
-    });
-    _timer ??= sampleTimer;
-    _timer!.reset();
+    _timer.reset();
   }
 
   void stop() {
     _stopwatch.stop();
-    _timer!.cancel();
+    _timer.cancel();
     notifyListeners();
   }
 }
